7d7272c146ea1082c8d2c897f95d1fe8323916a3
Matthieu_vm <mattbws93@gmail.com>
2023-05-13 23:58:12 +0200

    ajout machine , personnel

--- projet/projet/settings.py
+++ projet/projet/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "projetApp.apps.ProjetappConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- projet/projetApp/models.py
+++ projet/projetApp/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
+from datetime import datetime
 
 # Create your models here.
+
+class Machine(models.Model):
+
+	TYPE = {
+		('PC', ('PC - Windows')),
+		('Mac', ('Mac - MacOS')),
+		('Serveur', ('Serveur')),
+		('Switch', ('Switch')),	
+	}
+
+	id = models.AutoField(
+			primary_key =  True,
+			editable = False)
+
+	nom = models.CharField(
+		max_length = 15)
+
+	maintenance_date = models.DateField(default = datetime.now())
+
+	mach = models.CharField(max_length = 32, choices = TYPE, default = 'PC')
+
+	def __str__(self):
+		return str(self.id) + " -> " + self.nom
+
+	def get_name(self):
+		return str(self.id) + " " + self.nom
+
+
+
+
+
+
+class Personnel(models.Model):
+	id = models.PositiveIntegerField(
+		primary_key = True,
+		editable = True,
+		validators=[MaxValueValidator(99)]
+	)
+
+	nom = models.CharField(
+		max_length = 30
+	)
+
+	prenom = models.CharField(
+		max_length = 30
+	)
